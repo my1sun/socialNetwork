34b7c261ad2122dbb9e1a98a74b43554ea85ac93
Viacheslav <my1sun@yandex.ru>
2020-03-24 10:55:58 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className="header">
+    <img src="https://i.pinimg.com/originals/69/a8/04/69a804c8143949464484240e358b3c63.png" />
+  </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className="content">
+    <div>
+      <img src="https://avatars.mds.yandex.net/get-zen_doc/44645/pub_5a8b08a3a86731486c96c74c_5a8b0ca9581669462bbf5b6f/scale_1200" />
+    </div>
+    <div>
+      ava + desription
+    </div>
+    <div>
+      My post
+    </div>
+    <div>
+      New post
+    </div>
+    <div>
+      post 1
+    </div>
+    <div>
+      post 2
+    </div>
+  </div>
+}
+
+export default Profile;
